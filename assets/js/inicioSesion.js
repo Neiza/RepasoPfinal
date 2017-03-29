0b00aeac9e0a2139379d3cd0e61ba8481c141ebc
Neiza Nunez <neizapf@gmail.com>
2017-03-29 13:50:44 -0500

    inicioSesion

---
+++ assets/js/inicioSesion.js
@@ -0,0 +1,18 @@
+var registrado = document.getElementById("registrado");
+var registrate = document.getElementById("registrate");
+var correo = document.getElementById("correo");
+var contraeña = document.getElementById("contraseña")
+
+window.addEventListener("load", function(){
+  correo.disabled = true;
+  contraseña.disabled = true;
+      registrado.addEventListener("click", function(){
+              correo.disabled = false;
+              contraseña.disabled = false;
+              correo.focus();
+      });
+
+      registrate.addEventListener("click", function(){
+          window.location = "registro.html"
+      });
+});
