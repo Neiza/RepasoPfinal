bf77fcd8ef52532279c3abf2f6ab5a11d05f7f45
Neiza Nunez <neizapf@gmail.com>
2017-03-29 14:51:07 -0500

    inicias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 var botonRegistrar = document.getElementById("registrar");
+var inicia = document.getElementById("sesion");
 
 function registrar(){
       window.location = "registro.html"
+}
+
+function inicia (){
+  window.location = "inicioSesion.html"
 }
 
 window.addEventListener("load", function(){
       botonRegistrar.addEventListener("click", function(event){
               event.preventDefault();
               registrar();
+      });
+      inicia.addEventListener("click",function(event){
+              event.preventDefault();
+              inicia ();
       });
 });
