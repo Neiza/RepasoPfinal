b7cf26b8338843caa42895e30b4d51da160e0c72
Neiza Nunez <neizapf@gmail.com>
2017-03-29 14:57:13 -0500

    g

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -17,8 +17,6 @@ function ObjetoCoder(nombre, apellido, email, password){
         this.password = password;
 }
 
-
-
 function validarNombreApellido (){
         var soloLetras = function(e){
           var codigoTecla=e.keyCode;
@@ -35,44 +33,51 @@ function validarNombreApellido (){
 
 function validarEmailPassword(){
           var expressionEmail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-          var expressionPassword6Characters  =/^[A-Za-z0-9_]{6,12}$/;
+          var expressionPassword  =/^[A-Za-z0-9_]{6,20}$/;
 
           if (!expressionEmail.test(inputEmail.value)){
               alert("Ingresa un e-mail válido");
               }
-          if (!expressionEmail.test(inputEmail.value)){
-             alert("La contraseña de tener de 6 a 12 caracteres");
+          if (!expressionPassword.test(inputPassword.value)){
+             alert("La contraseña de tener de 6 a 20 caracteres");
              }
-    /*         var validacionInput = function(){
-             		if(this.value.trim().length==0){
-             			this.value ="";
-             			alert("*Este campo es obligatorio") ;
-             		}
-             		/*		var datoCorrecto = this.value.charAt(0).toUpperCase()+this.value.substring(1).toLowerCase();
-             		this.value=datoCorrecto;
-             		if(this.getAttribute("type")=="text"){
-             			var arrDato = this.value.split(" ");
-             			var datoConMayusculas = "";
-             			for(var i = 0; i<arrDato.length;i++){
-             				datoConMayusculas += arrDato[i].charAt(0).toUpperCase() + arrDato[i].substring(1).toLowerCase() + " ";
-             			}
-             			this.value=datoConMayusculas.trim();
-             		}
-             	}*/
-}
+  }
+
+
+  function validaciones(){
+        var inputs = document.getElementsByClassName("inputs");
+            for (var i = 0; i<inputs.length; i++){
+                if(inputs[i].value.trim().length==0){
+                    alert("LLene todos los campos")
+                }
+                break;
+            }
+    }
+
 
 
 window.addEventListener("load",function(){
       validarNombreApellido();
       botonRegistro.addEventListener("click", function(event){
             event.preventDefault();
+            validaciones()
             validarEmailPassword();
             ObjetoCoder(nombre,apellido,email,password);
-            new ObjetoCoder (inputNombre.value, inputApellido. value, inputEmail.value, inputPassword.value);
-            console.log(new ObjetoCoder(inputNombre.value, inputApellido.value, inputEmail.value, inputPassword.value))
-			      localStorage.setItem("nuevoCoder",JSON.stringify(new ObjetoCoder(inputNombre.value,inputApellido.value,inputEmail.value,inputPassword.value)))
+            var nombre = inputNombre.value.toUpperCase();
+            var apellido = inputApellido.value.toUpperCase();
+            var email = inputEmail.value;
+            var password = inputPassword.value ;
+            new ObjetoCoder (nombre, apellido, email, password);
+            if(nombre.length!=0 && apellido.length!=0 && email.length!=0 && password.length!=0){
+
+            console.log(new ObjetoCoder(nombre, apellido, email, password))
+			      localStorage.setItem("nuevoCoder",JSON.stringify(new ObjetoCoder(nombre,apellido,email,password)))
 		        document.getElementById("form").reset();
             window.location = "bienvenidaCoder.html"
+          }
+          else {
+            alert("Llenar todos los campos")
+          }
       });
 
 });
